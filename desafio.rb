a02c2311372e5f1614986e035716e712f57e12eb
WallaceWin <144985186+WallaceWin@users.noreply.github.com>
2023-09-14 01:43:44 -0400

    Update desafio.rb

--- desafio.rb
+++ desafio.rb
@@ -1,1 +1,10 @@
+puts "Digite seu nome"
+nome = gets.chomp
 
+puts "Digite seu sobrenome"
+sobrenome = gets.chomp
+
+puts "Digite sua idade"
+idade = gets.chomp
+
+puts "Seu nome é #{nome} #{sobrenome} e sua idade é #{idade}"
